9fe9687e6894533729e8fccb4d2a2d56e91ef504
haoxle <hao-le@live.co.uk>
2023-01-06 19:51:48 +0000

    made a window with a button counter

--- src/main/java/org/example/Gui.java
+++ src/main/java/org/example/Gui.java
@@ -1,4 +1,41 @@
 package org.example;
 
-public class Gui {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Gui implements ActionListener {
+    private int count;
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel label;
+
+    public Gui() {
+        frame = new JFrame();
+        panel = new JPanel();
+        JButton button = new JButton("Click me");
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: 0");
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("our Gui");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        new Gui();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
 }
